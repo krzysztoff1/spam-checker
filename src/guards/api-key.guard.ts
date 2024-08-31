0668589830d32f2f16450ee6329859d0c0b50092
Krzysztof <halidu13@gmail.com>
2024-08-31 13:09:57 +0200

    feat: add ApiKeyGuard

---
+++ src/guards/api-key.guard.ts
@@ -0,0 +1,23 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const apiKey = request.headers['x-api-key'];
+
+    if (!process.env.API_KEY || !apiKey || apiKey !== process.env.API_KEY) {
+      throw new UnauthorizedException('Invalid API key');
+    }
+
+    return true;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import { ApiKeyGuard } from './guards/api-key.guard';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalGuards(new ApiKeyGuard());
   await app.listen(3000);
 }
 bootstrap();
